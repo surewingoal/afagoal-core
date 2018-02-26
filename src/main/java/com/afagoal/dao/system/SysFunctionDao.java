74027d364d46005ef578abe73f2ebf2058c66041
xiaxi <xiaxi@jianbaolife.com>
2018-02-26 12:38:10 +0800

    登录

--- src/main/java/com/afagoal/dao/system/SysFunctionDao.java
+++ src/main/java/com/afagoal/dao/system/SysFunctionDao.java
@@ -6,7 +6,6 @@ import com.afagoal.entity.system.SysFunction;
 import com.querydsl.core.types.dsl.BooleanExpression;
 
 import org.springframework.stereotype.Repository;
-import org.springframework.util.CollectionUtils;
 
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -40,5 +39,21 @@ public class SysFunctionDao extends BaseDao<SysFunction, QSysFunction> {
         booleanExpressions.add(this.getQEntity().id.eq(id));
         SysFunction function = this.getEntity(booleanExpressions);
         return function;
+    }
+
+    public List<SysFunction> userFunction(Integer userId) {
+        String sql = "select f.* from sys_function f \n" +
+                "left join sys_role_function rf \n" +
+                "on rf.function_id = f.id\n" +
+                "left join sys_role r\n" +
+                "on rf.role_id = r.id\n" +
+                "left join sys_user_role ur \n" +
+                "on r.id = ur.role_id\n" +
+                "left join sys_user u\n" +
+                "on u.id = ur.user_id\n" +
+                "where u.id = (?1)";
+        Query query = this.getEntityManager().createNativeQuery(sql,SysFunction.class);
+        query.setParameter(1,userId);
+        return query.getResultList();
     }
 }
