04e3f05c6b17d2facac7b80222141220e48267f8
xiaxi <xiaxi@jianbaolife.com>
2018-06-11 20:54:02 +0800

    用户关注

--- src/main/java/com/afagoal/dao/blockchain/userFollow/TokenUserFollowDao.java
+++ src/main/java/com/afagoal/dao/blockchain/userFollow/TokenUserFollowDao.java
@@ -1,10 +1,10 @@
 package com.afagoal.dao.blockchain.userFollow;
 
-import com.afagoal.constant.BaseConstant;
 import com.afagoal.dao.BaseDao;
 import com.afagoal.entity.blockchain.userFollow.QTokenUserFollow;
 import com.afagoal.entity.blockchain.userFollow.TokenUserFollow;
 import com.querydsl.core.types.dsl.BooleanExpression;
+import com.querydsl.jpa.impl.JPAQuery;
 
 import java.util.List;
 
@@ -26,11 +26,26 @@ public class TokenUserFollowDao extends BaseDao<TokenUserFollow, QTokenUserFollo
         this.setQEntity(QTokenUserFollow.tokenUserFollow);
     }
 
-    public TokenUserFollow findByTokenId(String tokenId, Integer userId) {
+    public TokenUserFollow findByTokenIdAndUser(String tokenId, Integer userId) {
         List<BooleanExpression> booleanExpressions = new ArrayList();
         booleanExpressions.add(this.getQEntity().state.eq(FOLLOW_STATE));
         booleanExpressions.add(this.getQEntity().userId.eq(userId));
         booleanExpressions.add(this.getQEntity().tokenId.eq(tokenId));
         return this.getEntity(booleanExpressions);
+    }
+
+    public List<TokenUserFollow> findByTokenId(String tokenId) {
+        List<BooleanExpression> booleanExpressions = new ArrayList();
+        booleanExpressions.add(this.getQEntity().state.eq(FOLLOW_STATE));
+        booleanExpressions.add(this.getQEntity().tokenId.eq(tokenId));
+
+        JPAQuery<TokenUserFollow> query = new JPAQuery<>(this.getEntityManager());
+
+        query.select(this.getQEntity())
+                .from(this.getQEntity())
+                .leftJoin(this.getQEntity().user).fetchJoin()
+                .where(booleanExpressions.toArray(new BooleanExpression[booleanExpressions.size()]));
+
+        return query.fetch();
     }
 }
--- src/main/java/com/afagoal/entity/blockchain/userFollow/TokenUserFollow.java
+++ src/main/java/com/afagoal/entity/blockchain/userFollow/TokenUserFollow.java
@@ -1,9 +1,14 @@
 package com.afagoal.entity.blockchain.userFollow;
 
 import com.afagoal.entity.UuidEntity;
+import com.afagoal.entity.system.SysDept;
+import com.afagoal.entity.system.SysUser;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
 import javax.persistence.Table;
 
 import lombok.Getter;
@@ -19,6 +24,7 @@ import lombok.Setter;
 @Setter
 public class TokenUserFollow extends UuidEntity {
 
+    @Column(name = "user_id")
     private Integer userId;
 
     @Column(name = "bc_token_id")
@@ -26,5 +32,9 @@ public class TokenUserFollow extends UuidEntity {
 
     @Column(name = "bc_token_code")
     private String tokenCode;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id", insertable = false, updatable = false)
+    private SysUser user;
 
 }
