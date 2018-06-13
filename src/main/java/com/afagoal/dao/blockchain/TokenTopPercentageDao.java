928f7d425d63c8e4cede3aca851d302e422862c7
xiaxi <xiaxi@jianbaolife.com>
2018-06-13 16:13:13 +0800

    value  column

--- src/main/java/com/afagoal/dao/blockchain/TokenTopPercentageDao.java
+++ src/main/java/com/afagoal/dao/blockchain/TokenTopPercentageDao.java
@@ -2,12 +2,15 @@ package com.afagoal.dao.blockchain;
 
 import com.afagoal.constant.BaseConstant;
 import com.afagoal.dao.BaseDao;
+import com.afagoal.dto.base.ValueDateModel;
 import com.afagoal.entity.blockchain.QTokenTopPercentage;
 import com.afagoal.entity.blockchain.TokenTopPercentage;
+import com.afagoal.utils.db.CustomAliasResultTransformer;
 import com.querydsl.core.types.dsl.BooleanExpression;
 
 import java.util.List;
 
+import org.hibernate.Session;
 import org.springframework.stereotype.Repository;
 
 import java.util.ArrayList;
@@ -27,5 +30,24 @@ public class TokenTopPercentageDao extends BaseDao<TokenTopPercentage, QTokenTop
         expressions.add(this.getQEntity().state.ne(BaseConstant.DELETE_STATE));
         expressions.add(this.getQEntity().id.eq(id));
         return this.getEntity(expressions);
+    }
+
+    public List<ValueDateModel> valueDateModels(int size, String tokenId, byte topType) {
+        String sql = "select " +
+                "percentage as value, " +
+                "statistic_time as statisticTime " +
+                "from bc_token_top_percentage " +
+                "where state <> 99 " +
+                "and bc_token_id = ? " +
+                "and top_type = ? " +
+                "order by statistic_time desc " +
+                "limit ? ;";
+        Session session = (Session) this.getEntityManager().getDelegate();
+        org.hibernate.Query query = session.createSQLQuery(sql)
+                .setResultTransformer(new CustomAliasResultTransformer(ValueDateModel.class));
+        query.setParameter(0, tokenId);
+        query.setParameter(1, topType);
+        query.setParameter(2, size);
+        return query.list();
     }
 }
